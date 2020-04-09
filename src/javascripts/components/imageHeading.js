c9ff7871a774d65de07f1f6196f80048a2c95463
Sarah Holder <sr.holder@yahoo.com>
2020-04-09 15:31:14 -0500

    wip

--- src/javascripts/components/imageHeading.js
+++ src/javascripts/components/imageHeading.js
@@ -4,7 +4,7 @@ import './imageHeading.scss';
 const printImageHeading = () => {
   let domString = '';
   domString += '<div class="imgbox">';
-  domString += '<img class="img-fluid rounded mx-auto d-block" src="https://tinyurl.com/uz6h5jv" alt="Sarah Holder">';
+  domString += '<img class="img-fluid rounded mx-auto d-block" src="https://tinyurl.com/sarahHolderWebsitePicture" alt="Sarah Holder">';
   domString += '</div>';
   utils.printToDom('printImageDiv', domString);
 };
